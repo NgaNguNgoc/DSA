363c3c3c2b61608955b654f86a548b97210b1915
NgaNguNgoc <102353884+NgaNguNgoc@users.noreply.github.com>
2022-07-14 16:09:11 +0700

    Add files via upload

--- dat_ten.cpp
+++ dat_ten.cpp
@@ -1,52 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int n, k, ok;
-int a[17];
-
-void ktao()
+int n, k, pos = -1, gt[20];
+vector<string> s;
+string a[20];
+void sinh(int i)
 {
-    for (int i = 1; i <= k; i++)
-    {
-        a[i] = i;
-    }
-}
-void sinh()
-{
-    int i = k;
-    while (i >= 1 && a[i] == n - k + i)
-    {
-        i--;
-    }
-    if (i == 0)
-        ok = 0;
-    else
-    {
-        a[i]++;
-        for (int j = i + 1; j <= k; j++)
+    for (int j = 0; j < n; ++j)
+        if (j > pos && s[j] > a[i - 1])
         {
-            a[j] = a[j - 1] + 1;
+            pos = j;
+            a[i] = s[j];
+            if (i == k)
+            {
+                for (int h = 1; h <= i; ++h)
+                    cout << a[h] << " ";
+                cout << endl;
+            }
+            else
+                sinh(i + 1);
+            pos = -1;
         }
-    }
 }
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
+    cin >> n >> k;
+    a[0] = " ";
+    set<string> se;
+    while (n--)
     {
-        cin >> n >> k;
-        ok = 1;
-        ktao();
-        while (ok)
-        {
-            for (int i = 1; i <= k; i++)
-            {
-                char x = a[i] + 16 + '0';
-                cout << x;
-            }
-            cout << endl;
-            sinh();
-        }
+        string x;
+        cin >> x;
+        se.insert(x);
     }
+    for (auto x : se)
+        s.push_back(x);
+    n = se.size();
+    sinh(1);
 }
